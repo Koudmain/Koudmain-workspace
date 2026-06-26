f56ceb9dd7754eb01ce40efd1991be70b283d785
yanismgt <yanis.mignot@epitech.eu>
2026-06-26 11:27:41 +0200

    fix: script useless part

--- setup-documenso.sh
+++ setup-documenso.sh
@@ -11,19 +11,6 @@ echo -e "${BLUE}==================================================${NC}"
 echo -e "${BLUE}   Initialisation de Documenso      ${NC}"
 echo -e "${BLUE}==================================================${NC}"
 
-echo -e "\n${YELLOW}Vérification des certificats de signature...${NC}"
-
-if [ -f "key.pem" ] && [ -f "cert.pem" ] && [ -f "cert.p12" ]; then
-    echo -e "${GREEN}Les fichiers cert.pem, key.pem et cert.p12 existent déjà. Passage à l'étape suivante.${NC}"
-else
-    echo -e "${YELLOW}Certificats manquants. Génération d'un nouveau certificat de test...${NC}"
-
-    openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365 -nodes -subj "/CN=Koudmain Dev" && \
-    openssl pkcs12 -export -out cert.p12 -inkey key.pem -in cert.pem -passout pass:password
-
-    echo -e "${GREEN}Certificats créés avec succès !${NC}"
-fi
-
 echo -e "\n${YELLOW}Reset et injection des données de test...${NC}"
 
 docker exec -it koudmain-documenso npm run prisma:migrate-reset --workspace=@documenso/prisma
